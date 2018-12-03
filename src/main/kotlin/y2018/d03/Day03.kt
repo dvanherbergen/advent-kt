2eb08db5bd5871306eb6946aae2832c00df6e44d
Davy Vanherbergen <davy.vanherbergen@gmail.com>
2018-12-03 21:13:24 +0100

    beautification...

---
+++ src/main/kotlin/y2018/d03/Day03.kt
@@ -0,0 +1,42 @@
+import java.io.File
+
+typealias Coordinate = Pair<Int, Int>
+
+data class Claim(val id: Int, val left: Int, val top: Int, val width: Int, val height: Int) {
+
+    fun squares(): List<Coordinate> {
+        val s = ArrayList<Coordinate>()
+        for (i in left until (left + width)) {
+            for (j in top until (top + height)) {
+                s.add(Coordinate(i, j))
+            }
+        }
+        return s
+    }
+}
+
+fun main(args: Array<String>) {
+
+    val regex = """#(\d*) @ (\d*),(\d*): (\d*)x(\d*)""".toRegex()
+
+    val claims = File("src/main/resources/y2018/d03/input.txt").readLines()
+            .map { regex.matchEntire(it)!!.destructured
+                    .let { (a, b, c, d, e) -> Claim(a.toInt(), b.toInt(), c.toInt(), d.toInt(), e.toInt()) }}
+    val overlaps = claims
+            .flatMap { it.squares() }
+            .groupingBy { it }
+            .eachCount()
+            .filter { it.value > 1 }
+
+    println("Part 1: result = ${overlaps.count()}")
+
+    val claim = claims.find {
+        overlaps.keys.containsNone(it.squares())
+    }
+
+    println("Part 2: result = $claim")
+}
+
+fun <T> Collection<T>.containsNone(input: Collection<T>): Boolean {
+    return input.find { this.contains(it) } == null
+}
--- src/main/kotlin/y2018/d04/Part2.kt
+++
@@ -1,5 +0,0 @@
-package y2018.d04
-
-fun main(args: Array<String>) {
-    println("TODO")
-}
