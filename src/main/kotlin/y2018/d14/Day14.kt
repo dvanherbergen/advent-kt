a3b5047d4c35e1ce5228655d57486c602a13b05b
Davy Vanherbergen <davy.vanherbergen@gmail.com>
2018-12-14 08:07:20 +0100

    v2 day 14

--- src/main/kotlin/y2018/d14/Day14.kt
+++ src/main/kotlin/y2018/d14/Day14.kt
@@ -4,8 +4,11 @@ fun main() {
 
 
     val input = mutableListOf<Int>(3,7)
+//    val input = mutableListOf<Int>(9,9,0,9,4,1)
     val elfPositions = mutableListOf<Int>(0, 1)
-    var recipesCreated = 0
+            //    val elfPositions = mutableListOf<Int>(0, 1, 2, 3, 4, 5)
+
+    val start = 990941
 
     println(input)
     while (true) {
@@ -16,15 +19,12 @@ fun main() {
             score += input[it % input.size]
         }
 
-        if (score > 9) {
-            val score1 = score / 10
-            input.add(score1)
-           // recipesCreated ++
+
+        score.toString().forEach {
+            input.add(("" + it).toInt())
         }
 
-        val score2 = score % 10
-        input.add(score2)
-        recipesCreated ++
+
 
       //  println("Position ${elfPositions[0]} + to move (${(1 +  input[elfPositions[0]])}) = ${((elfPositions[0] + 1 +  input[elfPositions[0]]) % input.size)}")
         for (i in 0 until elfPositions.size) {
@@ -32,25 +32,60 @@ fun main() {
 
         }
 
-       // println(input)
-       // println("Positions aft $elfPositions")
-      //  println("")
-        for (i in 0 until input.size) {
-            if (i == elfPositions[0]) {
-                print("(${input[i]}) ")
-            } else if (i == elfPositions[1]) {
-                print("[${input[i]}] ")
-            } else {
-                print(" ${input[i]}  ")
-            }
-        }
-        println("")
+//        for (i in 0 until input.size) {
+//            if (i == elfPositions[0]) {
+//                print("(${input[i]}) ")
+//            } else if (i == elfPositions[1]) {
+//                print("[${input[i]}] ")
+//            } else {
+//                print(" ${input[i]}  ")
+//            }
+//        }
+//        println("")
 
-       if (input.size >= 2018 + 10) {
+       if (input.size >= start + 10) {
            break;
        }
     }
 
-    println(input.takeLast(10).joinToString(""))
+
+
+
+    println(input)
+    // 3230474240
+
+    println(input.slice(start until start+10).joinToString(""))
+
+
+    while (true) {
+
+
+        var score = 0
+        elfPositions.forEach {
+            score += input[it % input.size]
+        }
+
+
+        score.toString().forEach {
+            input.add(("" + it).toInt())
+        }
+
+
+
+        //  println("Position ${elfPositions[0]} + to move (${(1 +  input[elfPositions[0]])}) = ${((elfPositions[0] + 1 +  input[elfPositions[0]]) % input.size)}")
+        for (i in 0 until elfPositions.size) {
+            elfPositions[i] = ((elfPositions[i] + 1 +  input[elfPositions[i]]) % input.size)
+
+        }
+
+
+        if ((input[input.size - 1]) == 1) {
+            if (input.takeLast(6).joinToString("") == "990941") {
+                println("took ${input.size - 6}")
+                break;
+            }
+        }
+    }
+//153723222
 
 }
